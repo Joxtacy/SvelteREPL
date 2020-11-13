40bfb37b993a0345d8a41f54b8fbda02d3630cb9
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2020-11-13 20:37:48 +0900

    Remove bundled files from commits

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ import livereload from "rollup-plugin-livereload";
 import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 
-
 const production = !process.env.ROLLUP_WATCH;
 
 const onwarn = ({ message }) =>

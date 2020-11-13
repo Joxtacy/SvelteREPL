766432243a40ff0adcdd22758eabc7e9ad8980a3
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2020-11-13 20:55:15 +0900

    Make tab create spaces

--- src/Input.svelte
+++ src/Input.svelte
@@ -10,6 +10,21 @@
     $: currentComponentId = components.findIndex(({ id }) => id === current);
     $: tabs = components.map(({ id, name, type }) => ({ id, name, type }));
 
+    function keydownHandler(event: KeyboardEvent) {
+        if (event.key == "Tab") {
+            event.preventDefault();
+            var start = textarea.selectionStart;
+            var end = textarea.selectionEnd;
+
+            // set textarea value to: text before caret + tab + text after caret
+            const spaceTab = `    `;
+            textarea.value =`${textarea.value.substring(0, start)}${spaceTab}${textarea.value.substring(end)}`;
+
+            // put caret at right position again
+            textarea.selectionStart = textarea.selectionEnd = start + spaceTab.length;
+        }
+    }
+
     function getMax(_components: Component[]): number {
         const ids = _components.map(({ id }) => id);
         return Math.max(...ids);
@@ -41,6 +56,7 @@
         on:select={({ detail }) => (current = detail)}
         on:new={newComponent} />
     <textarea
+        on:keydown={keydownHandler}
         bind:value={components[currentComponentId].source}
         bind:this={textarea} />
 </section>
